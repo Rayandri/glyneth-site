a673ec88b5068d662d9b3ee70eb8d4132a11c737
Glyneth <glyneth@Glyneths-MacBook-Air.local>
2025-04-07 11:41:21 +0200

    updating

---
+++ src/js/main.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const buttons = document.querySelectorAll("[data-target]");
+  
+    buttons.forEach(button => {
+      button.addEventListener("click", () => {
+        const targetId = button.getAttribute("data-target");
+        const targetElement = document.getElementById(targetId);
+        if (targetElement) {
+          targetElement.classList.toggle("hidden");
+        }
+      });
+    });
+  });
+  
